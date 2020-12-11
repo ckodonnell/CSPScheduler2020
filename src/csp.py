0ee04327a88095f624662c2981bba3b3dba2c1e7
DC David <dcdavid@tutanota.com>
2020-12-11 16:19:12 -0500

    Added noTimeOverlap constraint for lab times. Including a bug

--- src/csp.py
+++ src/csp.py
@@ -51,17 +51,20 @@ class CSP():
         for time in self.domains[first][0]:
             for room in self.domains[first][1]:
                 for prof in self.domains[first][2]:
-                    local_assignment = assignment.copy()
-                    local_assignment[first] = [time, room, prof]
-                    # if we're still consistent, we recurse (continue)
-                    if self.consistent(first, local_assignment):
-                        result = self.backtracking_search(local_assignment)
-                        # if we didn't find the result, we will end up backtracking
-                        if result is not None:
-                            return result
-                            #self.solutionList.append(result)
-        #triply nested forloop lol
+                    for labtime in self.domains[first][3]:
+                        local_assignment = assignment.copy()
+                        local_assignment[first] = [time, room, prof, labtime]
+                        # if we're still consistent, we recurse (continue)
+                        if self.consistent(first, local_assignment):
+                            result = self.backtracking_search(local_assignment)
+                            # if we didn't find the result, we will end up backtracking
+                            if result is not None:
+                                return result
+                                #self.solutionList.append(result)
+        #quadruply nested forloop lol
         return None
+
+
 
 class connectedClasses(Constraint): #classes that people usually take together should be @ different times
     def __init__(self, class1, class2):
@@ -178,7 +181,7 @@ class classesThatDoNotNeedComputers(Constraint):
                 if assignment[c][1] not in self.rooms:
                     return False
         return True
-
+        
 
 class noTwoConsecutive(Constraint): #this is not finished...,,,
     def __init__(self, class1, class2, times):
@@ -190,6 +193,38 @@ class noTwoConsecutive(Constraint): #this is not finished...,,,
     def satisfied(self, assignment):
         return True
 
+# takes in two times
+# returns a boolean whether the two times overlap
+# check if a 1hr15 class block overlaps with a 2-hour lab block
+def noTimeOverlap(Constraint):
+    def __init__(self, classes):
+        super().__init__(classes)
+        
+    def satisfied(self, assignment):
+        """
+        a time string consists of:
+        time[0] -> "M"
+        time[1]    -> " "
+        time[2:]   -> "00:00"
+        """
+        def checkOverlap(time, labtime):
+            timeSplit = time.split()
+            labtimeSplit = labtime.split()
+            if labtimeSplit[0] == timeSplit[0][0] or labtimeSplit == timeSplit[0][2]:
+                if labtimeSplit[1] == "9:00" and (timeSplit[1] == "9:00" or timeSplit[1] == "10:30"):
+                    return False
+                elif labtimeSplit[1] == "1:00" and (timeSplit[1] == "12:00" or timeSplit[1] == "1:30"):
+                    return False
+                elif labtimeSplit[1] == "3:10" and (timeSplit[1] == "3:10" or timeSplit[1] == "4:35"):
+                    return False
+            return True
+        
+        for c in assignment.values():
+            if c[3] is not None:
+                if not checkOverlap(c[0], c[3]):
+                    return False
+        return True
+
 if __name__ == "__main__":
     professors = ["Gordon", "Hunsberger", "Smith", "Waterman", "Gommerstadt", "Lemieszewski", "Ellman", "Lambert", "Saravanan", "Williams"]
     classes = ["101-51", "101-52", "101-53", "101-54", "102-51", "102-52", "144", "145-51", "145-52", "195", "203", "224", "240", "241", "334"]
@@ -197,14 +232,16 @@ if __name__ == "__main__":
     times = ["M/W 9:00", "M/W 10:30", "M/W 12:00", "M/W 1:30", 
              "T/R 9:00", "T/R 10:30", "T/R 12:00", "T/R 1:30", "T/R 3:10", "T/R 4:35", 
              "W/F 9:00", "W/F 10:30", "W/F 12:00", "W/F 1:30"]
-    labs = [] 
-            
+    labtimes = ["T 9:00", "F 9:00", 
+                "M 1:00", "T 1:00", "W 1:00", "R 1:00", "F 1:00", 
+                "M 3:10", "T 3:10", "R 3:10", "F 3:10", 
+                "M 8:00", "T 8:00", "R 8:00"]
     VARIABLES = classes
     #DOMAINS = [professors, classes, rooms, times]
     domain = {}
     i = 0
     for clas in classes: 
-        domain[clas] = [times, rooms, professors] #so a list of lists...
+        domain[clas] = [times, rooms, professors, labtimes] #so a list of lists...
 
     # CONSTRAINTS START HERE
     scheduler = CSP(VARIABLES, domain)
@@ -220,15 +257,18 @@ if __name__ == "__main__":
 
     scheduler.add_constraint(assignClassToProfessor(["Hunsberger", "Gordon", "Smith"], classes[0:4])) # 101
     scheduler.add_constraint(assignClassToProfessor(["Gommerstadt", "Lemieszewski", "Ellman"], classes[4:6])) # 102
-    scheduler.add_constraint(assignClassToProfessor(["Waterman"], ["144", "224"])) # 144, 224
-    scheduler.add_constraint(assignClassToProfessor(["Gordon", "Lambert", "Lemieszewski"], classes[7:9])) # 145
+    scheduler.add_constraint(assignClassToProfessor(["Waterman"], ["144", "224"])) # 144
+    scheduler.add_constraint(assignClassToProfessor(["Ellman", "Gordon", "Lambert", "Lemieszewski"], classes[7:9])) # 145
+    scheduler.add_constraint(assignClassToProfessor(["Saravanan"], ["195"])) # 195
     scheduler.add_constraint(assignClassToProfessor(["Saravanan"], ["203"])) # 203
+    scheduler.add_constraint(assignClassToProfessor(["Waterman"], ["224"])) # 224
     scheduler.add_constraint(assignClassToProfessor(["Gordon", "Lambert"], ["240"])) # 240
     scheduler.add_constraint(assignClassToProfessor(["Waterman", "Williams"], ["334"])) # 334
     
     scheduler.add_constraint(classesThatNeedComputers(["101-51", "101-52", "101-53", "101-54", "224", "334"], ["SP309"]))
     scheduler.add_constraint(classesThatDoNotNeedComputers(["102-51", "102-52", "144", "145-51", "145-52", "195", "203", "240", "241"], ["SP105", "SP201", "SP206", "SP212"]))
 
+    scheduler.add_constraint(noTimeOverlap(classes))
     # TODO: optimize this so that it doesn't do O(n^2)
     for c1 in classes:
         for c2 in classes:
@@ -246,8 +286,6 @@ if __name__ == "__main__":
         print(json.dumps(result, indent=4, sort_keys=True))
 
 # TODO: fix -> all classes are MW
-# TODO: constraint for classes that need computers (for rooms)
-# TODO: maybe constraint for professors with 2 sections of a class (i.e. 101-51 and 101-52 are both Gordon)
 # TODO: add labs/intensives, asprey lab intensives
 # TODO: add more class preferences
 
